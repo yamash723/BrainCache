8e2c3b962b813d12c0c76195c73a6ad6865a78b2
Shuhei Yamashita <b.e7472@gmail.com>
2025-04-12 10:57:35 +0900

    Initial Commit

---
+++ quick-memo/quick_memoApp.swift
@@ -0,0 +1,32 @@
+//
+//  quick_memoApp.swift
+//  quick-memo
+//
+//  Created by 山下秀平 on R 7/04/12.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct quick_memoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
